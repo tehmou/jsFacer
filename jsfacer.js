c3ac84014784c6a4ba38e71ca14a9df300e40da7
Timo Tuominen <timo.tuominen@gmail.com>
2011-09-13 18:19:37 +0200

    Added some tests and fixed bugs.

--- jsfacer.js
+++ jsfacer.js
@@ -1,33 +1,41 @@
 var jsFacer = {
-    
-    interfaces: {},
-    
-    types: {
-        "function": _.isFunction,
-        "array": _.isArray,
-        "string": _.isString,
-        "boolean": _.isBoolean,
-        "number": _.isNumber,
-        "object": function () { return true; }
-    },
+    create: function (base) {
+        _.extend(base || this, {
+            interfaces: {},
+            types: {
+                "function": _.isFunction,
+                "array": _.isArray,
+                "string": _.isString,
+                "boolean": _.isBoolean,
+                "number": _.isNumber,
+                "any": function () { return true; }
+            },
 
-    define: function (name, interface) {
-        if (this.interfaces.hasOwnProperty(name)) {
-            throw "Interface " + name + " was already defined!";
-        }
-        this.interfaces[name] = interface;
-    },
+            define: function (name, interface, override) {
+                if (!override && this.interfaces.hasOwnProperty(name)) {
+                    throw "Interface \"" + name + "\" was already defined!";
+                }
+                this.interfaces[name] = interface;
+            },
 
-    check: function (object, interface) {
-        var realInterface = _.isString(interface) ? this.interfaces[interface] : interface;
-        var passed = true;
+            check: function (object, interface, acceptNull) {
+                var realInterface = _.isString(interface) ? this.interfaces[interface] : interface;
+                var passed = true;
+                var types = this.types;
 
-        _.each(realInterface, function (value, index) {
-            passed = passed && object.hasOwnProperty(index) && value(object[index]);
+                _.each(realInterface, function (value, index) {
+                    passed = passed && object.hasOwnProperty(index) && ((acceptNull && object[index] === null) || types[value](object[index]));
+                });
+                return passed;
+            },
+
+            assert: function (object, interface, acceptNull) {
+                if (!this.check.apply(this, arguments)) {
+                    throw "Object failed to implement interface \"" + interface + "\"!";
+                }
+            }
         });
-        
-        if (!passed) {
-            throw "Object failed to implement interface " + interface + "!";
-        }
     }
 };
+
+jsFacer.create();
---
+++ spec/spec_jsfacer.js
@@ -0,0 +1,128 @@
+describe("jsFacer", function () {
+    var completeInterface, originalValidTestObject, _jsFacer;
+
+    beforeEach(function () {
+        jsFacer.create(_jsFacer = {});
+        completeInterface = {
+            myFunction: "function",
+            myArray: "array",
+            myString: "string",
+            myBoolean: "boolean",
+            myNumber: "number",
+            myObject: "any"
+        };
+        originalValidTestObject = {
+            myFunction: function () { },
+            myArray: [],
+            myString: "string",
+            myBoolean: false,
+            myNumber: 52356,
+            myObject: {}
+        };
+    });
+
+    describe("Testing valid objects", function () {
+        
+        it("Should pass all known types", function () {
+            _jsFacer.assert(originalValidTestObject, completeInterface);
+        });
+
+        it("Should allow all types for type any", function () {
+            var validTestObject;
+            _.each(completeInterface, function (value, index) {
+                validTestObject = _.clone(originalValidTestObject);
+                validTestObject.myObject = originalValidTestObject[index];
+                _jsFacer.assert(validTestObject, completeInterface);
+            });
+        });
+
+        it("Should allow null when so specified", function () {
+            var validTestObject;
+            _.each(originalValidTestObject, function (value, index) {
+                validTestObject = _.clone(originalValidTestObject);
+                validTestObject[index] = null;
+                _jsFacer.assert(validTestObject, completeInterface, true);
+            });
+        });        
+    });
+
+    describe("Throwing errors", function () {
+
+        it("Should fail if one property is unimplemented", function () {
+            var errorThrown, invalidTestObject;
+            _.each(originalValidTestObject, function (value, index) {
+                errorThrown = false;
+                invalidTestObject = _.clone(originalValidTestObject);
+                delete invalidTestObject[index];
+                try {
+                    _jsFacer.assert(invalidTestObject, completeInterface);
+                } catch (e) {
+                    errorThrown = true;
+                }
+                expect(errorThrown).toBeTruthy();
+            });
+        });
+
+        it("Should fail if one property is of wrong type", function () {
+            var errorThrown, invalidTestObject, lastValue = "not a function";
+            _.each(originalValidTestObject, function (value, index) {
+                if (completeInterface[index] === "any") {
+                    return;
+                }
+                errorThrown = false;
+                invalidTestObject = _.clone(originalValidTestObject);
+                invalidTestObject[index] = lastValue;
+                try {
+                    _jsFacer.assert(invalidTestObject, completeInterface);
+                } catch (e) {
+                    errorThrown = true;
+                }
+                expect(errorThrown).toBeTruthy();
+                lastValue = value;
+            });
+        });
+
+        it("Should not allow null by default", function () {
+            var errorThrown, invalidTestObject;
+            _.each(originalValidTestObject, function (value, index) {
+                if (completeInterface[index] === "any") {
+                    return;
+                }
+                errorThrown = false;
+                invalidTestObject = _.clone(originalValidTestObject);
+                invalidTestObject[index] = null;
+                try {
+                    _jsFacer.assert(invalidTestObject, completeInterface);
+                } catch (e) {
+                    errorThrown = true;
+                }
+                expect(errorThrown).toBeTruthy();
+            });
+        });
+    });
+    
+    describe("Saving interfaces with name", function () {
+        
+        beforeEach(function () {
+            _jsFacer.define("myInterface", completeInterface);
+        });
+        
+        it("Should read the saved interface", function () {
+            _jsFacer.check(originalValidTestObject, "myInterface");
+        });
+        
+        it("Should not allow overriding an interface already defined", function () {
+            var errorThrown = false;
+            try {
+                _jsFacer.define("myInterface", {});
+            } catch (e) {
+                errorThrown = true;
+            }
+            expect(errorThrown).toBeTruthy();
+        });
+        
+        it("Should allow overriding an interface if forced", function () {
+            _jsFacer.define("myInterface", {}, true);
+        });
+    });
+});
